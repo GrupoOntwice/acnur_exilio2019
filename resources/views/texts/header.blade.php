5121ee74f02f999939aa11b10d8f752196844c18
Pablo <pilj@virket.com>
2019-06-07 00:07:02 -0500

    new template

---
+++ resources/views/texts/header.blade.php
@@ -0,0 +1,6 @@
+El 14 de diciembre de 1950 al término de la Segunda Guerra Mundial, se creó el ACNUR para ayudar a
+los millones de europeos desplazados por el conflicto. La Asamblea General de Naciones Unidas otorgó a
+ACNUR un mandato de tres años para completar sus labores y luego disolverse. Hoy, 69 años después,
+nuestra organización continúa con la labor de proteger y asistir a los refugiados alrededor del mundo. En
+México, el ACNUR tiene presencia desde 1982 y ha apoyado a los exiliados de las dictaduras en América
+Latina en los años 1980 y de la Guerra Civil en Centroamérica en los años 1990.
---
+++ resources/views/texts/videos/rafael.blade.php
@@ -0,0 +1,15 @@
+Rafael Urrusti Alonso<br><br>
+Por Pierre-Marc René<br><br><br>
+Rafael Urrusti Alonso es descendiente de una familia que huyó de España en 1939 durante la Guerra
+Civil y la dictadura de Francisco Franco. A sus 61 años y al cumplirse 80 aniversario del exilio español,
+el reconocido artista reclama que se declare ‘ejemplo de la humanidad, al embajador Gilberto
+Bosques, quien organizó por orden del presidente Lázaro Cárdenas la acogida de decenas de miles
+de refugiados españoles y europeos en México.<br><br>
+“Lo que significan Gilberto Bosques y la política de México del presidente Lázaro Cárdenas es la
+diferencia entre la vida y la muerte para decenas de miles de personas. Yo siempre he tenido la
+inquietud de honrarlo más como se merece, si es que hay manera de medir eso. De salvar a
+tantísimas miles de vida, no necesariamente refugiados españoles, porque cualquiera que fuera un
+ser humano y que necesitara refugio, acogida y protección (…) Estaban México, Gilberto Bosques y
+Lázaro Cárdenas”, declara el flautista.<br><br>
+Si bien Urrusti Alonso no vivió directamente el exilio español, su familia le ha contado cómo
+aconteció. Su abuelo materno trabajaba en Telégrafos ....
